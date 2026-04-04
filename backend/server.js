8b467601c567137e257d400cfab5d966d1759e1d
surajpandey111 <125928082+surajpandey111@users.noreply.github.com>
2026-04-04 17:43:11 +0530

    Fixed Firebase env config

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const admin = require("firebase-admin");
 
 // 🔥 FIREBASE INIT
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
