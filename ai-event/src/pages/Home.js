f9d8bbeaa63451f27fffdffcd0d26ac6e8479d1f
surajpandey111 <125928082+surajpandey111@users.noreply.github.com>
2026-04-16 16:19:55 +0530

    Fix React routing for admin page

--- ai-event/src/pages/Home.js
+++ ai-event/src/pages/Home.js
@@ -125,11 +125,11 @@ const Home = () => {
       {/* 🔥 PRIZES */}
       <h2> Prizes & Certification</h2>
       <ul style={{ textAlign: "left" }}>
-        <li> 1st Prize – Trophy  + Certificate(special recognization)</li>
-        <li> 2nd Prize – Trophy  + Certificate(special recognization)</li>
-        <li> 3rd Prize – Trophy  + Certificate(special recognization)</li>
+        <li> 1st Prize – Trophy  + Certificate(special recognization) + Special Gift</li>
+        <li> 2nd Prize – Trophy  + Certificate(special recognization) + Special Gift</li>
+        <li> 3rd Prize – Trophy  + Certificate(special recognization) + Special Gift</li>
         <li> Quiz Winners</li>
-        <li> Certificate for All</li>
+        <li> Certificate for All & Mentorship for all Participants</li>
       </ul>
 
 
